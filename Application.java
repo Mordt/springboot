d8ceff25a86336121602337f03ce982f98daadd0
Carlos Quintero <0xcquintero@gmail.com>
2022-09-23 18:43:29 +1000

    springboot tutorial

---
+++ Application.java
@@ -0,0 +1,31 @@
+package com.example.springboot;
+
+import java.util.Arrays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.ApplicationContext;
+import org.springframework.boot.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+    @bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx){
+        return args -> {
+            System.out.println("Lets inspect the beans created by Spring Boot:");
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            
+            Arrays.sort(beanNames);
+            for(String beanName : beanNames){
+                System.out.println(beanName);
+            }
+        };
+    }
+
+}
